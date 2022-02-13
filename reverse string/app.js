3d5535b913b525176898e352a479d93b015842fb
ucurtmaavcısı <simsekmerve1717@gmail.com>
2022-02-13 22:14:21 +0300

    çalılma

---
+++ reverse string/app.js
@@ -0,0 +1,11 @@
+let reverseWord=document.querySelector("#reverse");
+let button=document.querySelector("#button");
+let result=document.querySelector("#result");
+
+button.addEventListener("click",()=>{
+    
+    
+  
+    result.innerText=reverseWord.value.split("").reverse().join("");
+    
+})
