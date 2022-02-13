545613aff9d0e389bccc1c75a8603fa07fca8a80
ucurtmaavcısı <simsekmerve1717@gmail.com>
2022-02-13 23:10:29 +0300

    çalışma

---
+++ capital letter/app.js
@@ -0,0 +1,10 @@
+let input=document.querySelector("#text");
+let result=document.querySelector(".result");
+let button=document.querySelector(".button");
+
+button.addEventListener("click",()=>{
+    
+})
+
+
+
